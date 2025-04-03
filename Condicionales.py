202d71f1c0d90d3e9d1b5f20cd1d61adddb695b3
5CG9386HF5\Onizea <genefernandz@gmail.com>
2025-04-03 14:29:58 +0200

    creando archivo con ejercicios

---
+++ Condicionales.py
@@ -0,0 +1,132 @@
+# Ejercicios sobre
+#  Condicionales en Python
+#  Ejercicio 1
+#  Dada una edad ingresada por el usuario, determina si es mayor de edad (18 o más) o menor de edad (menos de 18).
+
+
+
+# Ejercicio 2
+#  Pide al usuario que ingrese un número de tres dígitos. Imprime si es un número par o impar.
+
+
+
+# Ejercicio 3
+#  Un usuario ingresa un salario mensual. Si el salario es mayor o igual a $2000, imprime "Salario alto", si es menor a
+#  $2000 pero mayor a $1000, imprime "Salario medio", y si es menor a $1000, imprime "Salario bajo".
+
+
+
+# Ejercicio 4
+#  Solicita un número entre 1 y 12 al usuario e imprime el nombre del mes correspondiente.
+
+
+
+# Ejercicio 5
+#  Un estudiante ingresa su puntaje en un examen. Si el puntaje es mayor o igual a 90, imprime "Aprobado con Distinción".
+#  Si es mayor o igual a 70 pero menor a 90, imprime "Aprobado". Si es menor a 70, imprime "Reprobado".
+
+
+# Ejercicio 7
+#  Ingresa un valor numérico para representar una temperatura en grados Celsius y determina si está bajo cero, entre 0 y
+#  30 grados, o por encima de 30 grados.
+
+
+
+
+# Ejercicio 8
+#  Si un número ingresado por el usuario es mayor que 100, imprímelo dividido entre 2. Si es menor o igual a 100,
+#  imprímelo multiplicado por 2.
+
+
+
+# Ejercicio 9
+#  Un alumno introduce las horas de trabajo semanal. Si las horas son más de 40, imprime "Trabajo a tiempo completo", si
+#  son menos de 40, imprime "Trabajo a medio tiempo".
+  
+
+# Ejercicio 10
+#  Pide la edad y determina si es un niño (menor de 12), un adolescente (entre 12 y 18), un adulto joven (entre 19 y 30), si
+#  es un adulto (mayor de 30) o si es persona perteneciente al a tercera edad(65).
+
+
+
+
+
+# Ejercicio 11
+#  Solicita un número de calificación entre 0 y 10. Si la calificación es mayor o igual a 9, imprime "Excelente". Si es mayor o
+#  igual a 7 pero menor a 9, imprime "Bueno". Si es menor a 7, imprime "Necesita mejorar".
+ 
+
+# Ejercicio 12
+#  Pide al usuario que ingrese un número. Si el número es mayor que 50, imprime su mitad. Si es menor o igual a 50,
+#  imprímelo multiplicado por 3.
+
+
+# Ejercicio 13
+#  Solicita al usuario que ingrese un valor en minutos y determina cuántas horas y minutos son.
+ 
+ 
+
+# Ejercicio 14
+#  Pide un número y determina si es un número positivo, negativo o cero.
+  
+ 
+
+ 
+# Ejercicio 15
+#  Ingresa la hora actual en formato de 24 horas. Si es de día (de 6 a 18), imprime "Es de día". Si es de noche (de 18 a 6),
+#  imprime "Es de noche".
+ 
+ 
+ 
+
+ 
+# Ejercicio 16
+#  Un usuario ingresa su edad y su altura. Si la edad es mayor de 18 y la altura mayor de 1.70 metros, imprime "Puedes
+#  entrar a la montaña rusa". Si no, imprime "No puedes entrar".
+  
+ 
+ 
+ 
+ 
+ 
+
+# Ejercicio 17
+# Pide al usuario que ingrese dos números. Si el primero es mayor que el segundo, imprime el primero. Si el segundo es
+#  mayor que el primero, imprime el segundo. Si son iguales, imprime "Son iguales".
+
+ 
+ 
+ 
+# Ejercicio 18
+#  Solicita al usuario que ingrese un valor en segundos. Si los segundos son mayores a 3600, imprímelo como horas y
+#  minutos. Si son menores o iguales a 3600, imprímelo como minutos y segundos.
+  
+ 
+ 
+ 
+ 
+# Ejercicio 19
+#  Ingresa un número de 4 dígitos. Si el número es divisible entre 3, imprime "Divisible entre 3". Si no es divisible entre 3,
+#  imprime "No divisible entre 3".
+  
+
+ 
+ 
+ 
+# Ejercicio 20
+#  Si el precio de un producto es mayor a 1000, aplica un descuento del 10%. Si es menor o igual a 1000, aplica un
+#  descuento del 5%. Imprime el precio final después del descuento.
+  
+ 
+ 
+ 
+# Ejercicio 21
+#  Determina si un año es bisiesto. Un año es bisiesto si es divisible entre 4, pero no divisible entre 100, excepto si también
+#  es divisible entre 400.
+
+
+ 
+ 
+# Ejercicio 22
+#  Pide un número e imprime si el número es menor que 0, igual a 0, mayor a 100 o mayor a 0 pero menor a 100
