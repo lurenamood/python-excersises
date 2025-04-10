70ffb5cb55eab12882a4ba6de7fbec69fbf88063
5CG9386HF5\Onizea <genefernandz@gmail.com>
2025-04-10 14:49:34 +0200

    Avance en los ejercicios FOR

---
+++ for.py
@@ -0,0 +1,115 @@
+# 📝 Ejercicios con for en Python
+
+#FACTORIAL
+
+
+#1
+for n in range(1, 6):
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    print(f"El factorial de {n} es {factorial}")
+    
+    
+    
+#2
+numeros = [5, 4, 3, 2, 1]
+
+for n in numeros:
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    print(f"El factorial de {n} es {factorial}")
+    
+    
+    
+#3
+import math
+print(math.factorial(5))
+
+
+
+
+# Imprimir números del 1 al 20
+# # Muestra en pantalla los números del 1 al 20, uno por línea.
+
+
+
+
+# Sumar los primeros 100 números
+# Usa un bucle for para sumar todos los números del 1 al 100 e imprime el resultado.
+
+
+
+
+
+# Tabla de multiplicar
+# Pide al usuario un número y muestra su tabla de multiplicar del 1 al 10.
+
+
+
+
+
+# Números pares del 1 al 50
+# Imprime solo los números pares del 1 al 50.
+
+
+
+
+
+
+
+# Contar letras 'a' en una palabra
+#Pide al usuario una palabra y cuenta cuántas veces aparece la letra 'a'.
+
+
+
+
+
+
+
+# Imprimir una lista al revés
+# Dada una lista de números, imprímela en orden inverso usando for.
+
+
+
+
+
+
+# Factorial de un número
+# Pide al usuario un número entero positivo y calcula su factorial con un for.
+
+
+
+
+
+# Contar vocales en una frase
+# Pide una frase y cuenta cuántas vocales (a, e, i, o, u) tiene.
+
+
+
+
+
+
+# Crear una pirámide de asteriscos
+# Usa un bucle for para imprimir una pirámide así:
+
+
+# markdown
+# Copiar
+# Editar
+# *
+# **
+# ***
+# ****
+# *****
+
+
+
+
+
+
+
+
+# Número más grande de una lista
+# Dada una lista de números, encuentra el número más grande sin usar max().
